8a374b53cc38835c72ee3922290b3ce458291f9d
AaronBDSouza <arrowknight007@gmail.com>
2021-09-22 22:11:59 +0530

    Updated CSS

--- src/index.js
+++ src/index.js
@@ -53,15 +53,22 @@ class Board extends React.Component{
 class Game extends React.Component{
   render(){
       return(
-          <div className="game">
-              <div className="game-board">
-                  <Board/>
-              </div>
-              <div className="game-info">
-                  <div>{/* status */}</div>
-                  <div>{/* TODO */}</div>
-              </div>
+          <div>
+            <div className="game-heading">
+                <h1>Tic-Tac-Toe</h1>   
+            </div>       
+            <div className="game">
+                <div className="game-board">
+                    <Board/>
+                </div>
+                <div className="game-info">
+                    <div>{/* status */}</div>
+                    <div>{/* TODO */}</div>
+                </div>
+            </div>
           </div>
+
+
       );
   }
 }
